a4022ee48273bcd22733df060ab832f981727d81
Beatrix <bealearnscode@gmail.com>
2019-04-01 14:48:00 +0000

    no. just no

---
+++ square.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+// Beatrix House
+
+// FILL IN THE CODE TO DECLARE A CLASS CALLED Square.  TO DO THIS SEE
+// THE IMPLEMENTATION SECTION.
+class Square
+{
+private:
+	float side;
+
+public:
+    void setSide(float length);
+    float findArea();
+    float findPerimeter();
+    Square (); // default constructor
+    Square (float length); // overload?
+   ~Square(); // destructor
+	
+};
+
+int main()
+{
+    Square  box;          // box is defined as an object of the Square class
+    float   size;         // size contains the length of a side of the square  
+
+   // FILL IN THE CLIENT CODE THAT WILL ASK THE USER FOR THE LENGTH OF THE SIDE 
+   // OF THE SQUARE.  (This is stored in size)
+   cout << "What is the length of the side of the square?" << endl;
+   cin >> box.side;
+   cout << endl;
+	
+   // FILL IN THE CODE THAT CALLS SetSide.
+   box.setSide(size);
+
+   // FILL IN THE CODE THAT WILL RETURN THE AREA FROM A CALL TO A FUNCTION
+   // AND PRINT OUT THE AREA TO THE SCREEN
+   cout << box.findArea();
+
+   // FILL IN THE CODE THAT WILL RETURN THE PERIMETER FROM A CALL TO A 
+   // FUNCTION AND PRINT OUT THAT VALUE TO THE SCREEN 
+   cout << box.findPerimeter();
+
+	return 0;
+}
+
+//__________________________________________________________________
+//Implementation section     Member function implementation
+
+//**************************************************
+//                  setSide
+//
+// task:    This procedure takes the length of a side and
+//          places it in the appropriate member data
+// data in: length of a side
+//***************************************************
+
+void Square::setSide(float length)
+ 
+{
+   side = length;	
+}
+//**************************************************
+//                 findArea
+//
+// task:    This finds the area of a square
+// data in: none (uses value of data member side)
+// data returned:  area of square
+//***************************************************
+
+float Square::findArea()
+{
+   return side * side;
+}
+//**************************************************
+//                 findPerimeter
+//
+// task:    This finds the perimeter of a square
+// data in: none (uses value of data member side)
+// data returned:  perimeter of square
+//***************************************************	
+float Square::findPerimeter()
+{
+   return 4 * side;
+}
